7737130fe03daad73ac318a35e41304550cee427
Shishir Shekhar <sspdav02@gmail.com>
2024-09-08 00:42:42 +0530

    chore(constant): create tasks list

---
+++ src/lib/contants.ts
@@ -0,0 +1,41 @@
+import { Task } from "./types";
+
+export const tasks: Task[] = [
+    {
+        status: "Todo",
+        priority: "High",
+        title: "Brainstorming",
+        description: "Brainstorming with the team for the new project",
+        dueDate: "09/09/2024",
+    }, {
+        status: "Done",
+        priority: "Medium",
+        title: "Wireframing",
+        description: "Creating wireframes for the new project",
+        dueDate: "09/09/2024",
+    }, {
+        status: "Todo",
+        priority: "Low",
+        title: "Designing",
+        description: "Designing the new project",
+        dueDate: "09/09/2024",
+    }, {
+        status: "In Progress",
+        priority: "High",
+        title: "Development",
+        description: "Developing the new project",
+        dueDate: "09/09/2024",
+    }, {
+        status: "In Progress",
+        priority: "Medium",
+        title: "Testing",
+        description: "Testing the new project",
+        dueDate: "09/09/2024",
+    }, {
+        status: "In Progress",
+        priority: "Low",
+        title: "Deployment",
+        description: "Deploying the new project",
+        dueDate: "09/09/2024",
+    }
+]

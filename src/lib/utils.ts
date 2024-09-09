d7aaba4b4a622162be28776484b66bba182c39b9
Shishir Shekhar <sspdav02@gmail.com>
2024-09-09 21:46:12 +0530

    refactor(utils): remove unneccessary code

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-// import { notification } from "antd";
-
-// // Helper function to handle error notifications
-// export const handleErrorNotification = (errorTitle: string, errorMessage: string) => {
-//     notification.error({
-//         message: errorTitle,
-//         description: errorMessage,
-//     });
-// };

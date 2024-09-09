43acaf4cf69860be733edc40d5c5ef44a85c5264
Shishir Shekhar <sspdav02@gmail.com>
2024-09-09 21:32:16 +0530

    feat(utils): create a function to handleErrorNotification

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+import { notification } from "antd";
+
+// Helper function to handle error notifications
+export const handleErrorNotification = (errorTitle: string, errorMessage: string) => {
+    notification.error({
+        message: errorTitle,
+        description: errorMessage,
+    });
+};

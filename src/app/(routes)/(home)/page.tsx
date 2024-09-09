b2f0d6736276fdd42c516b0213a90c38d378a0a7
Shishir Shekhar <sspdav02@gmail.com>
2024-09-09 22:36:47 +0530

    feat(page): fetch all tasks add modal state and optimize functions

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,26 +1,73 @@
-import { useMemo } from "react";
+"use client";
+
+import { useEffect, useMemo, useState, useCallback } from "react";
+import { notification, Spin } from "antd";
+import { Task } from "@/lib/types";
+import { getAllTasks } from "@/lib/firebase/functions/tasks";
 import CreateTaskBar from "@/components/home/CreateTaskBar/CreateTaskBar";
-import { tasks } from "@/lib/contants";
-import styles from "./Home.module.css";
 import TaskList from "@/components/home/TaskList/TaskList";
+import TaskModal from "@/components/ui/TaskModal/TaskModal";
+import styles from "./Home.module.css";
 
 export default function Home() {
-  const todoTasks = useMemo(
-    () => tasks.filter((task) => task.status === "Todo"),
-    [tasks]
-  );
-  const inProgressTasks = useMemo(
-    () => tasks.filter((task) => task.status === "In Progress"),
-    [tasks]
-  );
-  const doneTasks = useMemo(
-    () => tasks.filter((task) => task.status === "Done"),
-    [tasks]
-  );
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const tasks = await getAllTasks();
+        setTasks(tasks);
+      } catch (error) {
+        notification.error({
+          message: "Error fetching tasks",
+          description:
+            "An error occurred while fetching tasks. Please try again later.",
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  // Memoize task lists to improve performance
+  const { todoTasks, inProgressTasks, doneTasks } = useMemo(() => {
+    return {
+      todoTasks: tasks.filter((task) => task.status === "Todo"),
+      inProgressTasks: tasks.filter((task) => task.status === "In Progress"),
+      doneTasks: tasks.filter((task) => task.status === "Done"),
+    };
+  }, [tasks]);
+
+  // Open the modal
+  const openTaskModal = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  // Render loading state with a spinner
+  if (loading) {
+    return (
+      <div className={styles.loadingContainer}>
+        <Spin
+          size="large"
+          fullscreen={true}
+          tip="Loading tasks, please wait..."
+        />
+      </div>
+    );
+  }
 
   return (
     <main className={styles.home}>
-      <CreateTaskBar />
+      <TaskModal
+        open={isModalOpen}
+        setOpen={setIsModalOpen}
+        setTasks={setTasks}
+      />
+      <CreateTaskBar showModal={openTaskModal} />
 
       <div className={styles.container} aria-labelledby="task-lists">
         <section aria-label="Todo Tasks">

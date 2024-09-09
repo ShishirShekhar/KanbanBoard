d02d86c1713b5b4d2edc5af6c991739e6d7e3043
Shishir Shekhar <sspdav02@gmail.com>
2024-09-09 22:28:06 +0530

    refactor(type): update dueDate type to Date

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,7 +3,7 @@ export type BaseTask = {
     description: string;
     status: "Todo" | "In Progress" | "Done";
     priority: "High" | "Medium" | "Low";
-    dueDate: string;
+    dueDate: Date;
 };
 
 export type Task = BaseTask & {
